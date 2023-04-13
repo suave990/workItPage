057b35857a9483fd1b4e65a77d291a0727ec3769
suave990 <kezasuave990@gmail.com>
2023-04-13 13:48:20 -0700

    WorkIt Page Design

---
+++ index.js
@@ -0,0 +1,6 @@
+const learnButton = document.querySelector('button.learn')
+const div2 = document.querySelector('.div2')
+
+learnButton.addEventListener('click', (event) => {
+  div2.style.display = 'flex'
+})
